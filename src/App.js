72f4f416022c215b5a9d1337cffd848025ac7bf4
JeffAbney <jeffmabney@gmail.com>
2018-09-10 10:19:34 -0500

    add onKeyPress functionality and display styling

--- src/App.js
+++ src/App.js
@@ -22,54 +22,78 @@ class App extends Component {
 		}
 
 		this.handleClick = this.handleClick.bind(this);
-		this.handleKey = this.handleKey.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
 
+		this.drumKey = {
+			q: "Clap",
+			w: "hiHat",
+			e: "hiHat0",
+			a: "hiHat1",
+			s: "hiHat2",
+			d: "kick",
+			z: "lowTom1",
+			x: "lowTom2",
+			c: "snare",
+		}
 	}
+
+	componentDidMount() {
+    document.addEventListener('keypress', this.handleKeyPress);
+  }
+  	componentWillUnmount() {
+    document.removeEventListener('keypress', this.handleKeyPress);
+  }
 
 	handleClick(key){
 		this.setState({
 			sound: key,
+			input: this.drumKey[key],
 		});
 		document.getElementById(key).play();
 	}
 
-	handleKey(){
-		
+	handleKeyPress(event){
+		this.setState({
+			sound: event.key,
+			input: this.drumKey[event.key],
+		});
+		document.getElementById(event.key).play();
 	}
 
   render() {
+
     return (
       <div className="App" id="drum-machine">
         <div className="display" id="display">
         	<p>{this.state.input}</p>
         </div>
         <div className="drum-pad-container">
-        	<div className="drum-pad" onClick={() => {this.handleClick("Q")}} id="dp-q">Q
-        		<audio src={clap} className="clip" id="Q" preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("q")}} id="dp-q">Q
+        		<audio src={clap} className="clip" id="q" preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("W")}} id="dp-w">W
-        		<audio src={hiHat} className="clip" id="W" preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("w")}} id="dp-w">W
+        		<audio src={hiHat} className="clip" id="w" preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("E")}} id="dp-e">E
-        		<audio src={hiHat0} className="clip" id="E"  preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("e")}} id="dp-e">E
+        		<audio src={hiHat0} className="clip" id="e"  preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("A")}} id="dp-a">A
-        		<audio src={hiHat1} className="clip" id="A"  preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("a")}} id="dp-a">A
+        		<audio src={hiHat1} className="clip" id="a"  preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("S")}} id="dp-s">S
-        		<audio src={hiHat2} className="clip" id="S"  preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("s")}} id="dp-s">S
+        		<audio src={hiHat2} className="clip" id="s"  preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("D")}} id="dp-d">D
-        		<audio src={kick} className="clip" id="D"  preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("d")}} id="dp-d">D
+        		<audio src={kick} className="clip" id="d"  preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("Z")}} id="dp-z">Z
-        		<audio src={lowTom1} className="clip" id="Z"  preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("z")}} id="dp-z">Z
+        		<audio src={lowTom1} className="clip" id="z"  preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("X")}} id="dp-x">X
-        		<audio src={lowTom2} className="clip" id="X"  preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("x")}} id="dp-x">X
+        		<audio src={lowTom2} className="clip" id="x"  preload="auto"/>
         	</div>
-        	<div className="drum-pad" onClick={() => {this.handleClick("C")}} id="dp-c">C
-        		<audio src={snare} className="clip" id="C"  preload="auto"/>
+        	<div className="drum-pad" onClick={() => {this.handleClick("c")}} id="dp-c">C
+        		<audio src={snare} className="clip" id="c"  preload="auto"/>
         	</div>
         </div>
       </div>
